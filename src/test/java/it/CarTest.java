ef61a84f12fe74b0e4c7a94dc8a03fb07dd1a28b
Bruno Nogueira <ctw04264@criticaltechworks.com>
2025-05-06 16:27:50 +0100

    tirei comentarios yh

--- src/test/java/it/CarTest.java
+++ src/test/java/it/CarTest.java
@@ -1,4 +1,4 @@
-/* package it;
+package it;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -39,4 +39,4 @@ public class CarTest {
                         containsString("X5"));
     }
 
-} */
+}
